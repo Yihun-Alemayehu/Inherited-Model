8df51fc25ea7991847dacd5973259d8966da3737
Yihun Alemayehu <yankure01@gmail.com>
2023-11-19 21:50:29 +0300

    another commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show Random;
+import 'dart:developer' as devtools show log;
 
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -21,8 +24,148 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var color1 = Colors.yellow;
+  var color2 = Colors.blue;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        centerTitle: true,
+      ),
+      body: AvailableColorsWidget(
+        color1: color1,
+        color2: color2,
+        child: Column(
+          children: [
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      color1 = colors.getRandomElement() as MaterialColor;
+                    });
+                  },
+                  child: const Text('change color 1'),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      color2 = colors.getRandomElement() as MaterialColor;
+                    });
+                  },
+                  child: const Text('change color 2'),
+                ),
+              ],
+            ),
+            const ColorWidget(
+              color: AvailableColors.one,
+            ),
+            const ColorWidget(
+              color: AvailableColors.two,
+            ),
+          ],
+        ),
+      ),
+    );
   }
+}
+
+enum AvailableColors { one, two }
+
+class AvailableColorsWidget extends InheritedModel<AvailableColors> {
+  final MaterialColor color1;
+  final MaterialColor color2;
+
+  const AvailableColorsWidget({
+    Key? key,
+    required Widget child,
+    required this.color1,
+    required this.color2,
+  }) : super(
+          key: key,
+          child: child,
+        );
+
+  static AvailableColorsWidget of(
+    BuildContext context,
+    AvailableColors aspect,
+  ) {
+    return InheritedModel.inheritFrom<AvailableColorsWidget>(
+      context,
+      aspect: aspect,
+    )!;
+  }
+
+  @override
+  bool updateShouldNotify(covariant AvailableColorsWidget oldWidget) {
+    devtools.log('updateShouldNotify');
+    return color1 != oldWidget.color1 || color2 != oldWidget.color2;
+  }
+
+  @override
+  bool updateShouldNotifyDependent(
+    covariant AvailableColorsWidget oldWidget,
+    Set<AvailableColors> dependencies,
+  ) {
+    devtools.log('updateShouldNotifyDependent');
+
+    if (dependencies.contains(AvailableColors.one) &&
+        color1 != oldWidget.color1) {
+      return true;
+    }
+
+    if (dependencies.contains(AvailableColors.two) &&
+        color2 != oldWidget.color2) {
+      return true;
+    }
+
+    return false;
+  }
+}
+
+class ColorWidget extends StatelessWidget {
+  final AvailableColors color;
+  const ColorWidget({super.key, required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (color) {
+      case AvailableColors.one:
+        devtools.log('Color 1 widget is rebuilt !');
+      case AvailableColors.two:
+        devtools.log('Color 2 widget is rebuilt !');
+    }
+
+    final provider = AvailableColorsWidget.of(
+      context,
+      color,
+    );
+    return Container(
+      height: 100,
+      color: color == AvailableColors.one ? provider.color1 : provider.color2,
+    );
+  }
+}
+
+final colors = [
+  Colors.black,
+  Colors.yellow,
+  Colors.green,
+  Colors.blue,
+  Colors.amber,
+  Colors.orange,
+  Colors.red,
+  Colors.brown,
+  Colors.indigo,
+];
+
+extension RandomElements<T> on Iterable<T> {
+  T getRandomElement() => elementAt(
+        Random().nextInt(length),
+      );
 }
